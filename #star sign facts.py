a10f3bf18c11047d864c5bcd307f22314d0d7002
Apollo-0 <xthecharlieeffectx@gmail.com>
2022-01-21 19:19:44 +0000

    added error to elif statement

---
+++ #star sign facts.py
@@ -0,0 +1,72 @@
+#star sign facts
+
+import time
+print()
+print("*********************************************************")
+print("Welcome to star sign facts!")
+time.sleep(2)
+print("All you need to do is type your sign and we'll do the rest!")
+print()
+
+time.sleep(2)
+print("Firts let's do introductions!")
+time.sleep(1)
+print("Whats your name? :")
+name = input()
+print("Hello," ,name, "you can call us Apollo :)")
+if name =='apollo':
+    print("twins!1!!")
+elif name =='Apollo':
+    print("twins!1!!")
+
+time.sleep(2)
+print()
+print("Now we can get to buisness!")
+time.sleep(2)
+print("Make sure to type in: UPPERCASE")
+print("*********************************************************")
+time.sleep(2)
+print("Enter you sign here:")
+print()
+time.sleep(1)
+starsign = input()
+if starsign == 'ARIES':
+    print("You are born between:")
+
+elif starsign == 'GEMINI':
+    print("Firstly...")
+    time.sleep(1)
+    print("We're matching!")
+    time.sleep(1)
+    print("Secondly, you are born between:")
+    
+elif starsign == 'CANCER':
+    print("You are born between:")
+    
+elif starsign == 'LEO':
+    print("You are born between:")
+        
+elif starsign == 'VIRGO':
+    print("You are born between:")
+        
+elif starsign == 'LIBRA':
+    print("You are born between:")
+        
+elif starsign == 'SCORPIO':
+    print("You are born between:")
+        
+elif starsign == 'SAGGITARIUS':
+    print("You are born between:")
+        
+elif starsign == 'CAPRICORN':
+    print("You are born between:")
+        
+elif starsign == 'AQUARIUS':
+    print("You are born between:")
+        
+elif starsign == 'PISCES':
+    print("You are born between:")
+
+else:
+    print("It seems youve made an error :(")
+    time.sleep(1)
