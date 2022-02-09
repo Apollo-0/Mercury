5d22f1561de5f3633e775978f7169cf601a02c33
Apollo-0 <xthecharlieeffectx@gmail.com>
2022-02-09 22:41:29 +0000

    change locations

--- #cafe program.py
+++ #cafe program.py
@@ -1,5 +1,9 @@
 #cafe program
+
 import time
+cost = 0
+orders = []
+
 print("**************************************************")
 print("Welcome to Cloudy Cafe!")
 print()
@@ -23,58 +27,66 @@ while True:
         print("=========================================")
 
         time.sleep(1)
+        while True:
+            print("""
+            1) Fried rice with egg
+            2) Spaghetti Bolognese
+            3) Beef Steak
+            4)Chcken Salad
+            """)
 
-        print("""
-        1) Fried rice with egg
-        2) Spaghetti Bolognese
-        3) Beef Steak
-        4)Chcken Salad
-        """)
-
-        time.sleep(1)
-        fopt = input("Your Food Option is: ")
-
-        time.sleep(1)
-        if fopt == '1':
-            print("+++++++++++++++++++++++++++++++++++++++++")
-            print("You have chosen: Fried rice and egg")
             time.sleep(1)
-            print("This will cost: £4.99")
-            print("+++++++++++++++++++++++++++++++++++++++++")
-            print()
-            print("**************************************************")
+            fopt = input("Your Food Option is: ")
 
-
-        if fopt == '2':
-            print("+++++++++++++++++++++++++++++++++++++++++")
-            print("You have chosen: Spaghetti Bolognese")
             time.sleep(1)
-            print("This will cost: £5.45")
-            print("+++++++++++++++++++++++++++++++++++++++++")
-            print()
-            print("**************************************************")
+            if fopt == '1':
+                print("+++++++++++++++++++++++++++++++++++++++++")
+                print("You have chosen: Fried rice and egg")
+                orders.append('Fried rice and egg')
+                time.sleep(1)
+                print("This will cost: £4.99")
+                cost = cost + 4.99
+                print("+++++++++++++++++++++++++++++++++++++++++")
+                print()
+                print("**************************************************")
+                break 
 
 
-        if fopt == '3':
-            print("+++++++++++++++++++++++++++++++++++++++++")
-            print("You have chosen: Beef Steak")
-            time.sleep(1)
-            print("This will cost: £5.00")
-            print("+++++++++++++++++++++++++++++++++++++++++")
-            print()
-            print("**************************************************")
+            if fopt == '2':
+                print("+++++++++++++++++++++++++++++++++++++++++")
+                print("You have chosen: Spaghetti Bolognese")
+                orders.append('Spaghetti Bolognese')
+                time.sleep(1)
+                print("This will cost: £5.45")
+                cost = cost + 5.45
+                print("+++++++++++++++++++++++++++++++++++++++++")
+                print()
+                print("**************************************************")
+                break
 
+            if fopt == '3':
+                print("+++++++++++++++++++++++++++++++++++++++++")
+                print("You have chosen: Beef Steak")
+                orders.append('Beef Steak')
+                time.sleep(1)
+                print("This will cost: £5.00")
+                cost = cost + 5.00
+                print("+++++++++++++++++++++++++++++++++++++++++")
+                print()
+                print("**************************************************")
+                break
 
-        if fopt == '4':
-            print("+++++++++++++++++++++++++++++++++++++++++")
-            print("You have chosen: Chicken Salad")
-            time.sleep(1)
-            print("This will cost: £4.90")
-            print("+++++++++++++++++++++++++++++++++++++++++")
-            print()
-            print("**************************************************")
-
-        breakpoint()
+            if fopt == '4':
+                print("+++++++++++++++++++++++++++++++++++++++++")
+                print("You have chosen: Chicken Salad")
+                orders.append('Chicken Salad')
+                time.sleep(1)
+                print("This will cost: £4.90")
+                cost = cost + 4.90
+                print("+++++++++++++++++++++++++++++++++++++++++")
+                print()
+                print("**************************************************")
+                break
 
 
     if opt == 'b':
@@ -82,55 +94,64 @@ while True:
         print("You hav chosen: Snack Options")
         print("=========================================")
 
-        print("""
-        1) Turkey Sandwiches
-        2) French Fries
-        3) Crossaint
-        4) Fruit Salad""")
 
-        sopt = input("Your Snack Option is: ")
+        while True:
+            print("""
+            1) Turkey Sandwiches
+            2) French Fries
+            3) Crossaint
+            4) Fruit Salad""")
 
-        time.sleep(1)
-        if sopt == '1':
-            print("+++++++++++++++++++++++++++++++++++++++++")
-            print("You have chosen: Turey Sandwiches")
+            sopt = input("Your Snack Option is: ")
+
             time.sleep(1)
-            print("This will cost: £2.50")
-            print("+++++++++++++++++++++++++++++++++++++++++")
-            print()
-            print("**************************************************")
-
+            if sopt == '1':
+                print("+++++++++++++++++++++++++++++++++++++++++")
+                print("You have chosen: Turkey Sandwiches")
+                orders.append('Turkey Sandwiches')
+                time.sleep(1)
+                print("This will cost: £2.50")
+                cost = cost + 2.50
+                print("+++++++++++++++++++++++++++++++++++++++++")
+                print()
+                print("**************************************************")
+                break
     
-        if sopt == '2':
-            print("+++++++++++++++++++++++++++++++++++++++++")
-            print("You have chosen: French Fries")
-            time.sleep(1)
-            print("This will cost: £2.10")
-            print("+++++++++++++++++++++++++++++++++++++++++")
-            print()
-            print("**************************************************")
+            if sopt == '2':
+                print("+++++++++++++++++++++++++++++++++++++++++")
+                print("You have chosen: French Fries")
+                orders.append('French Fries')
+                time.sleep(1)
+                print("This will cost: £2.10")
+                cost = cost + 2.10
+                print("+++++++++++++++++++++++++++++++++++++++++")
+                print()
+                print("**************************************************")
+                break
 
-
-        if sopt == '3':
-            print("+++++++++++++++++++++++++++++++++++++++++")
-            print("You have chosen: Crossaint")
-            time.sleep(1)
-            print("This will cost: £1.50")
-            print("+++++++++++++++++++++++++++++++++++++++++")
-            print()
-            print("**************************************************")
-
+            if sopt == '3':
+                print("+++++++++++++++++++++++++++++++++++++++++")
+                print("You have chosen: Crossaint")
+                orders.append('Crossaint')
+                time.sleep(1)
+                print("This will cost: £1.50")
+                cost = cost + 1.50
+                print("+++++++++++++++++++++++++++++++++++++++++")
+                print()
+                print("**************************************************")
+                break
     
-        if sopt == '4':
-            print("+++++++++++++++++++++++++++++++++++++++++")
-            print("You have chosen: French Fries")
-            time.sleep(1)
-            print("This will cost: £2.00")
-            print("+++++++++++++++++++++++++++++++++++++++++")
-            print()
-            print("**************************************************")
-
-        breakpoint()
+            if sopt == '4':
+                print("+++++++++++++++++++++++++++++++++++++++++")
+                print("You have chosen: French Fries")
+                orders.append('French Fries')
+                time.sleep(1)
+                print("This will cost: £2.00")
+                cost = cost + 2.00
+                print("+++++++++++++++++++++++++++++++++++++++++")
+                print()
+                print("**************************************************")
+                break
 
 
     if opt == 'c':
@@ -138,55 +159,62 @@ while True:
         print("You hav chosen: Desert Options")
         print("=========================================")
 
-        print("""
-        1) Ice Cream
-        2) Caramel Cake
-        3) Chocolate Brownie
-        4) Strawberry Donut""")
+        while True:
+            print("""
+            1) Ice Cream
+            2) Caramel Cake
+            3) Chocolate Brownie
+            4) Strawberry Donut""")
 
-        dopt = input("Your Desert Option is: ")
+            dopt = input("Your Desert Option is: ")
 
-        time.sleep(1)
-        if dopt == '1':
-            print("+++++++++++++++++++++++++++++++++++++++++")
-            print("You have chosen: Ice Cream")
             time.sleep(1)
-            print("This will cost: £1.90")
-            print("+++++++++++++++++++++++++++++++++++++++++")
-            print()
-            print("**************************************************")
+            if dopt == '1':
+                print("+++++++++++++++++++++++++++++++++++++++++")
+                print("You have chosen: Ice Cream")
+                orders.append('Ice Cream')
+                time.sleep(1)
+                print("This will cost: £1.90")
+                cost = cost + 1.90
+                print("+++++++++++++++++++++++++++++++++++++++++")
+                print()
+                print("**************************************************")
+                break
+        
+            if dopt == '2':
+                print("+++++++++++++++++++++++++++++++++++++++++")
+                print("You have chosen: Caramel Cake")
+                orders.append('Caramel Cake')
+                time.sleep(1)
+                print("This will cost: £2.00")
+                cost = cost + 2.00
+                print("+++++++++++++++++++++++++++++++++++++++++")
+                print()
+                print("**************************************************")
+                break
 
-    
-        if dopt == '2':
-            print("+++++++++++++++++++++++++++++++++++++++++")
-            print("You have chosen: Caramel Cake")
-            time.sleep(1)
-            print("This will cost: £2.00")
-            print("+++++++++++++++++++++++++++++++++++++++++")
-            print()
-            print("**************************************************")
-
-
-        if dopt == '3':
-            print("+++++++++++++++++++++++++++++++++++++++++")
-            print("You have chosen: Chocolate Brownie")
-            time.sleep(1)
-            print("This will cost: £1.50")
-            print("+++++++++++++++++++++++++++++++++++++++++")
-            print()
-            print("**************************************************")
-
-    
-        if dopt == '4':
-            print("+++++++++++++++++++++++++++++++++++++++++")
-            print("You have chosen: Strawberry Donut")
-            time.sleep(1)
-            print("This will cost: £1.90")
-            print("+++++++++++++++++++++++++++++++++++++++++")
-            print()
-            print("**************************************************")
-
-        breakpoint()
+            if dopt == '3':
+                print("+++++++++++++++++++++++++++++++++++++++++")
+                print("You have chosen: Chocolate Brownie")
+                time.sleep(1)
+                print("This will cost: £1.50")
+                cost = cost + 1.50
+                print("+++++++++++++++++++++++++++++++++++++++++")
+                print()
+                print("**************************************************")
+                break
+        
+            if dopt == '4':
+                print("+++++++++++++++++++++++++++++++++++++++++")
+                print("You have chosen: Strawberry Donut")
+                orders.append('Strawberry Donut')
+                time.sleep(1)
+                print("This will cost: £1.90")
+                cost = cost + 1.90
+                print("+++++++++++++++++++++++++++++++++++++++++")
+                print()
+                print("**************************************************")
+                break
 
 
 
@@ -195,67 +223,103 @@ while True:
         print("You hav chosen: Drink Options")
         print("=========================================")
 
-        print("""
-        1) Milkshake
-        2) Smoothie
-        3) Hot Chocolate
-        4) Ice Tea
-        5) Soda""")
+        while True:
+            print("""
+            1) Milkshake
+            2) Smoothie
+            3) Hot Chocolate
+            4) Ice Tea
+            5) Soda""")
 
-        bopt = input("Your Drink Option is: ")
-    
-    #Putting bopt instead of dopt because then we'd have two and it would get mixed up with drink and desert
-    #The b in bopt just stands for beverage instead
+            bopt = input("Your Drink Option is: ")
+        
+            #Putting bopt instead of dopt because then we'd have two and it would get mixed up with drink and desert
+            #The b in bopt just stands for beverage instead
 
-        print()
-        time.sleep(1)
-        if bopt == '1':
-            print("+++++++++++++++++++++++++++++++++++++++++")
-            print("You have chosen: Milkshake")
-            time.sleep(1)
-            print("This will cost: £2.50")
-            print("+++++++++++++++++++++++++++++++++++++++++")
             print()
-            print("**************************************************")
-
-    
-        if bopt == '2':
-            print("+++++++++++++++++++++++++++++++++++++++++")
-            print("You have chosen: Smoothie")
             time.sleep(1)
-            print("This will cost: £2.45")
-            print("+++++++++++++++++++++++++++++++++++++++++")
-            print()
-            print("**************************************************")
+            if bopt == '1':
+                print("+++++++++++++++++++++++++++++++++++++++++")
+                print("You have chosen: Milkshake")
+                orders.append('Milkhake')
+                time.sleep(1)
+                print("This will cost: £2.50")
+                cost = cost + 2.50
+                print("+++++++++++++++++++++++++++++++++++++++++")
+                print()
+                print("**************************************************")
+                break
+        
+            if bopt == '2':
+                print("+++++++++++++++++++++++++++++++++++++++++")
+                print("You have chosen: Smoothie")
+                orders.append('Smoothie')
+                time.sleep(1)
+                print("This will cost: £2.45")
+                cost = cost + 2.45
+                print("+++++++++++++++++++++++++++++++++++++++++")
+                print()
+                print("**************************************************")
+                break
+
+            if bopt == '3':
+                print("+++++++++++++++++++++++++++++++++++++++++")
+                print("You have chosen: Hot Chocolate")
+                orders.append('Hot Chocolate')
+                time.sleep(1)
+                print("This will cost: £2.50")
+                cost = cost + 2.50
+                print("+++++++++++++++++++++++++++++++++++++++++")
+                print()
+                print("**************************************************")
+                break
+        
+            if bopt == '4':
+                print("+++++++++++++++++++++++++++++++++++++++++")
+                print("You have chosen: Ice Tea")
+                orders.append('Ice Tea')
+                time.sleep(1)
+                print("This will cost: £2.00")
+                cost = cost + 2.00
+                print("+++++++++++++++++++++++++++++++++++++++++")
+                print()
+                print("**************************************************")
+                break
+        
+            if bopt == '5':
+                print("+++++++++++++++++++++++++++++++++++++++++")
+                print("You have chosen: Soda")
+                orders.append('Soda')
+                time.sleep(1)
+                print("This will cost: £1.10")
+                cost = cost + 1.10
+                print("+++++++++++++++++++++++++++++++++++++++++")
+                print()
+                print("**************************************************")
+                break
 
 
-        if bopt == '3':
-            print("+++++++++++++++++++++++++++++++++++++++++")
-            print("You have chosen: Hot Chocolate")
+    if opt == 'V':
+        print("You've chosen to view your current items and cost")
+        print("Here are your items:")
+        for item in orders:
+            print(item)
+        print("Your current cost is: £" ,cost, " ")
+
+
+
+
+
+
+
+
+    if opt == 'Q':
+        print("You have Chosen to leave")
+
+        while True:
             time.sleep(1)
-            print("This will cost: £2.50")
-            print("+++++++++++++++++++++++++++++++++++++++++")
-            print()
-            print("**************************************************")
-
-    
-        if bopt == '4':
-            print("+++++++++++++++++++++++++++++++++++++++++")
-            print("You have chosen: Ice Tea")
-            time.sleep(1)
-            print("This will cost: £2.00")
-            print("+++++++++++++++++++++++++++++++++++++++++")
-            print()
-            print("**************************************************")
-
-    
-        if bopt == '5':
-            print("+++++++++++++++++++++++++++++++++++++++++")
-            print("You have chosen: Soda")
-            time.sleep(1)
-            print("This will cost: £1.10")
-            print("+++++++++++++++++++++++++++++++++++++++++")
-            print()
-            print("**************************************************")
-
-        breakpoint()
+            print("Items you have chosen:")
+            for item in orders:
+                print(item)
+            print("Your total amount is: £" ,cost, " ")
+            breakpoint()
